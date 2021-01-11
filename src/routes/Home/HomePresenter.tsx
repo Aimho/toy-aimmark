63db7e16a29342af7f52d1b93f102dae86affcf4
AimHo <jjhwhwnsgh@gmail.com>
2021-01-11 14:06:55 +0900

    Add firebase test

--- src/routes/Home/HomePresenter.tsx
+++ src/routes/Home/HomePresenter.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
+const HomeContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #000;
+  color: #fff;
+  font-size: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function Home() {
   return (
-    <div>
+    <HomeContainer>
       <h1>홈</h1>
-    </div>
+    </HomeContainer>
   );
 }
 
